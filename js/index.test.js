7265705d67ebf24ef76d3fa240d2dc352b073d25
Bastien Dumont <32489032+bastiendmt@users.noreply.github.com>
2021-12-30 16:57:06 +0100

    testing with puppeteer

---
+++ js/index.test.js
@@ -0,0 +1,66 @@
+import puppeteer from "puppeteer";
+
+let browser;
+const app =
+  "file://C:/Users/Bastien/Documents/Code/GitHub/classsed-jest/index.html";
+
+test("Validating first name field", async () => {
+  browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(app);
+
+  await page.click("input#firstName");
+  await page.type("input#firstName", " ");
+  await page.click("input#lastName");
+
+  let firstNameInputClass = await page.$eval(
+    "input#firstName",
+    (input) => input.className
+  );
+
+  expect(firstNameInputClass).toBe("invalid");
+
+  await page.click("input#firstName");
+  await page.type("input#firstName", "John");
+  await page.click("input#lastName");
+
+  firstNameInputClass = await page.$eval(
+    "input#firstName",
+    (input) => input.className
+  );
+
+  expect(firstNameInputClass).toBe("valid");
+  await browser.close();
+});
+
+test("Validating all fields", async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 35,
+  });
+  const page = await browser.newPage();
+  await page.goto(app);
+
+  await page.click("input#firstName");
+  await page.type("input#firstName", "John");
+
+  await page.click("input#lastName");
+  await page.type("input#lastName", "Doe");
+
+  await page.click("input#password");
+  await page.type("input#password", "123456a");
+
+  await page.click("input#confirmPassword");
+  await page.type("input#confirmPassword", "123456a");
+
+  await page.click("input#email");
+  await page.type("input#email", "john@email.com");
+
+  try {
+    const invalidInput = await page.$eval("input.invalid", (input) => input);
+    expect(invalidInput).toBeUndefined();
+  } catch (error) {
+    expect(error).toBeDefined();
+  }
+  await browser.close();
+}, 10000);
