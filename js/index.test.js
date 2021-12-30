0920ab30d9770df0be58a18e7958a1c90ba84c92
Bastien Dumont <32489032+bastiendmt@users.noreply.github.com>
2021-12-30 17:02:22 +0100

    clicking sending button

--- js/index.test.js
+++ js/index.test.js
@@ -64,3 +64,35 @@ test("Validating all fields", async () => {
   }
   await browser.close();
 }, 10000);
+
+test("fetching success panel", async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 40,
+    args: ["--window-size=1280,800"],
+  });
+
+  const page = await browser.newPage();
+  await page.goto(app);
+
+  await page.click("input#firstName");
+  await page.type("input#firstName", "John");
+
+  await page.click("input#lastName");
+  await page.type("input#lastName", "Doe");
+
+  await page.click("input#password");
+  await page.type("input#password", "123456a");
+
+  await page.click("input#confirmPassword");
+  await page.type("input#confirmPassword", "123456a");
+
+  await page.click("input#email");
+  await page.type("input#email", "john@email.com");
+
+  await page.click("button#formBtn");
+
+  let successPanel = await page.waitForSelector("div.card-panel");
+  expect(successPanel).toBeDefined();
+  await browser.close();
+}, 10000);
